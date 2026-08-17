6ac01ea91a3933ad7cd462322b54e16a0be4c5fd
sahupriyanka721-hub <sahupriyanka721@gmail.com>
2026-08-17 15:58:08 +0530

    commit

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,20 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBsrzCQjeffjsjEk0eMECv0e6e85tikolo",
+  apiKey: "AIzaSyBsrzCQjeffjSjEk0eMEcV0e6e85tikolo",
   authDomain: "univote-app-fc2cb.firebaseapp.com",
   projectId: "univote-app-fc2cb",
   storageBucket: "univote-app-fc2cb.firebasestorage.app",
   messagingSenderId: "1069409748155",
-  appId: "1:1069409748155:web:0b9dd43d7d5e8e82f5159e"
+  appId: "1:1069409748155:web:0b9dd43d7d5e8e82f5159e",
+  measurementId: "G-3GZ7SVZD01"
 };
 
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
