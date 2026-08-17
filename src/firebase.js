fd35002688f9d89db15e213f35d385659f1dd55b
sahupriyanka721-hub <sahupriyanka721@gmail.com>
2026-08-17 12:44:22 +0530

    Updated exact Firebase config with correct API key

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,13 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBsrzCQjeffjsjEk0eMEcV0e6e85tikolo",
+  apiKey: "AIzaSyBsrzCQjeffjSjEk0eMEcV0e6e85tikolo",
   authDomain: "univote-app-fc2cb.firebaseapp.com",
   projectId: "univote-app-fc2cb",
   storageBucket: "univote-app-fc2cb.firebasestorage.app",
@@ -12,8 +16,6 @@ const firebaseConfig = {
   measurementId: "G-3GZ7SVZD01"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
-export const googleProvider = new GoogleAuthProvider();
-export const db = getFirestore(app);
+const analytics = getAnalytics(app);
