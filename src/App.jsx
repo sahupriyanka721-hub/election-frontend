1e9419037e8ecd01a183fb862fe7b88ccc47036e
sahupriyanka721-hub <sahupriyanka721@gmail.com>
2026-08-25 15:12:33 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -81,8 +81,7 @@ export default function App() {
       eligible: '35,000+',
       status: 'Approved',
       docName: 'LPU_Registration_Bundle.pdf',
-      image: 'https://images.unsplash.com/photo-1541339907198-e08756dedf3f?auto=format&fit=crop&w=1200&q=80',      candidates: [
-        { id: 1, name: 'Simran Kaur', party: 'Panther Group', votes: 210 },
+      image: 'https://images.unsplash.com/photo-1541339907198-e08756dedf3f?auto=format&fit=crop&w=1200&q=80',        { id: 1, name: 'Simran Kaur', party: 'Panther Group', votes: 210 },
         { id: 2, name: 'Rohit Gupta', party: 'Students Voice', votes: 180 }
       ]
     }
